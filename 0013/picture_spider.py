84652a1c83082c682b436921c8658f2565a1bbc9
Miao Xuanyang <mxy134718@gmail.com>
2018-08-21 17:33:30 +0800

    添加一个新的方法

--- 0013/picture_spider.py
+++ 0013/picture_spider.py
@@ -18,5 +18,15 @@ def main():
             f.write(requests.get(list[i]['src']).content)
             f.close()
 
+# def main():
+#     page = requests.get(url).text
+#     doc = lxml.html.document_fromstring(page)
+#     for idx, el in enumerate(doc.cssselect('img.BDE_Image')):
+#         with open('%03d.jpg' % idx, 'wb') as f:
+#             f.write(requests.get(el.attrib['src']).content)
+#             f.close()
+
+
+
 if __name__ == '__main__':
     main()
